ce0afc6a1d7ece9d0e6ced6eadf21050998dfbc1
Maevskaya Olga <olganma@gmail.com>
2023-07-08 11:21:00 +0300

    Initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "g66qpu",
+    projectId: "bamt9j",
     baseUrl: 'http://qamid.tmweb.ru',
     setupNodeEvents(on, config) {
     },
